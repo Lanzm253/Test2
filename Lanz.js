53dedb96cf15be9f2706b80d568747c769914349
Lanzm253 <lanzm253@gmail.com>
2024-09-01 14:24:19 +0800

    Add files via upload

---
+++ Lanz.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.redirect-link');
+
+    // Define the link mappings
+    const linkMappings = {
+        1: 'https://mez.ink/pidcheas',
+        2: 'https://mez.ink/clark',
+        3: 'https://mez.ink/lanz'
+    };
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default link behavior
+            const linkId = this.getAttribute('data-link-id');
+            const url = linkMappings[linkId];
+            if (url) {
+                window.location.href = url; // Redirect to the URL
+            } else {
+                console.error('No URL found for link ID:', linkId);
+            }
+        });
+    });
+});
