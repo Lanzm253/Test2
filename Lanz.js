954350787af4faebf07cb486b52c1269b6e0507e
Lanzm253 <lanzm253@gmail.com>
2024-09-14 03:34:24 +0800

    Update Lanz.js

--- Lanz.js
+++ Lanz.js
@@ -7,9 +7,8 @@
         return; // Prevent further execution if not on the allowed domain
     }
 
-    // Your existing JS code goes here
     document.addEventListener('DOMContentLoaded', function() {
-        const links = document.querySelectorAll('.redirect-link');
+        const redirectButton = document.getElementById('redirectButton');
 
         // Define the link mappings
         const linkMappings = {
@@ -20,17 +19,14 @@
             5: 'https://mez.ink/Minecraft'
         };
 
-        links.forEach(link => {
-            link.addEventListener('click', function(event) {
-                event.preventDefault(); // Prevent the default link behavior
-                const linkId = this.getAttribute('data-link-id');
-                const url = linkMappings[linkId];
-                if (url) {
-                    window.location.href = url; // Redirect to the URL
-                } else {
-                    console.error('No URL found for link ID:', linkId);
-                }
-            });
+        redirectButton.addEventListener('click', function() {
+            const linkId = this.getAttribute('data-link-id');
+            const url = linkMappings[linkId];
+            if (url) {
+                window.location.href = url; // Redirect to the URL
+            } else {
+                console.error('No URL found for link ID:', linkId);
+            }
         });
     });
 })();
